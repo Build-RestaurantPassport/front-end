9caf5b813f219fb86cc8052b604e6d28b8988f37
fuston05 <fuston05@yahoo.com>
2020-02-01 12:59:04 -0500

    I installed react app, and router-dom, cleaned up starter file logo and all that, initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Cleaned up starter files and installed dependencies:</h1>
+      <p>react-router-dom</p>
+      <p>not sure if we're using styled-components or not</p>
+      <p>I would personally prefer styled-components or SASS</p>
+    </div>
+  );
+}
+
+export default App;
