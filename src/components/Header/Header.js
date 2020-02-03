17ae2b9b6c563cfdeb0414f0b6709957f0cc6144
fuston05 <fuston05@yahoo.com>
2020-02-03 17:18:37 -0500

    tweaked profile page. added right arrow to get to the next view 'restaurants' view

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 //components
 import profileHeaderImg from '../../Assets/images/Burger_Icon.png';
-
+import RightArrow from '../../Assets/images/chevron_right.png'
 //styles
 import {ProfileLogoImg, ProfileLogoImgCont} from './HeaderStyles';
 const Header = () => {
   return (
     <div className= 'headerCont'>
+      <Link to= 'localrestaurants'><img src= {RightArrow} /></Link>
       <a href= '#'>Edit Profile Cover</a>
       <ProfileLogoImgCont className= 'profileHeaderImgCont'>
         <ProfileLogoImg src= {profileHeaderImg} />
